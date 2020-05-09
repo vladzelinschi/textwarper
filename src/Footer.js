bac564fc2c385c4d502a5b7ea7ddb2f3a51bcdea
Vlad Zelinschi <the.reignn@gmail.com>
2020-05-09 18:36:58 +0300

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,13 +5,16 @@ const StyledFooter = styled.footer`
   text-align: center;
 `;
 
-const Footer = () => (
-  <StyledFooter>
-    Made with love by{' '}
-    <a href="https://www.vladzelinschi.com/" title="Vlad Zelinschi">
-      Vlad Zelinschi
-    </a>
-  </StyledFooter>
-);
+<StyledFooter>
+  Made with love by{' '}
+  <a href="https://www.vladzelinschi.com/" title="Vlad Zelinschi">
+    Vlad Zelinschi
+  </a>
+  . If you want, you can{' '}
+  <a href="https://github.com/vladzelinschi/textwarper">
+    check out the source code
+  </a>{' '}
+  of the project.
+</StyledFooter>
 
 export default Footer;
